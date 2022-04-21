3263fd8fff755a1dc8e1e831c23aee9522b81331
evnmck <evnmck@vt.edu>
2022-04-21 18:57:08 -0400

    user controller

--- src/GymRats/ProjectRunner.java
+++ src/GymRats/ProjectRunner.java
@@ -5,20 +5,34 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.Scanner;
 
 public class ProjectRunner {
-	
-	static String connectionUrl = "jdbc:mysql://127.0.0.1:3306/gymrats"; //could be different
-	static String dbUsername = "root"; //replace with your username (most likely "root")
-	static String dbPassword = "382682498Mck!"; //replace with your password 
+
+	static String connectionUrl = "jdbc:mysql://127.0.0.1:3306/gymrats"; // could be different
+	static String dbUsername = "root"; // replace with your username (most likely "root")
+	static String dbPassword = "382682498Mck!"; // replace with your password
 
 	public static void main(String[] args) throws ClassNotFoundException {
-	
 		UserController user = new UserController(connectionUrl, dbUsername, dbPassword);
-		user.getUsers();
-	
+		/*
+		 * Scanner scan = new Scanner(System.in); System.out.print("Enter User Info: ");
+		 * Dictionary<String, String> userInfo = new Hashtable<String, String>();
+		 * String[] all = scan.nextLine().split("\\s*,\\s*"); for(int i = 0; i <
+		 * all.length; i++) { String[] kv = all[i].split("\\s*:\\s*");
+		 * userInfo.put(kv[0], kv[1]); }
+		 * 
+		 * System.out.println(userInfo);
+		 */
+		// user.deleteUserByUserId(21);
+		ArrayList<Dictionary<String, String>> ret = user.getUsers();
+		for (int j = 0; j < ret.size(); j++) {
+			System.out.println(ret.get(j));
+		}
+
 	}
 
 	public static void getWorkoutForUser() throws ClassNotFoundException {
@@ -33,30 +47,28 @@ public class ProjectRunner {
 				ResultSet rs = ps.executeQuery()) {
 			while (rs.next()) {
 				int id = rs.getInt("User_Id");
-				if(id == user_id) {
-				String eName = rs.getString("Exercise_Name");
-				String name = rs.getString("Username");
-				String start = rs.getString("Start_Weight");
-				String end = rs.getString("End_Weight");
-				String reps = rs.getString("Repetitions");
-				String sets = rs.getString("Sets");
-				String time = rs.getString("Time_in_Minutes");
-				System.out.println("\nInfo for user " + id + ": " + name + "\nExercise: " + eName
-						+ "\nStarting Weight (lbs): " + start + "\nEnding Weight (lbs): " + end + "\nReps: " + reps
-						+ "\nSets: " + sets + "\nTotal Time: " + time);
-				found = true;
+				if (id == user_id) {
+					String eName = rs.getString("Exercise_Name");
+					String name = rs.getString("Username");
+					String start = rs.getString("Start_Weight");
+					String end = rs.getString("End_Weight");
+					String reps = rs.getString("Repetitions");
+					String sets = rs.getString("Sets");
+					String time = rs.getString("Time_in_Minutes");
+					System.out.println("\nInfo for user " + id + ": " + name + "\nExercise: " + eName
+							+ "\nStarting Weight (lbs): " + start + "\nEnding Weight (lbs): " + end + "\nReps: " + reps
+							+ "\nSets: " + sets + "\nTotal Time: " + time);
+					found = true;
 				}
 			}
-			
-			if(!found) {
-					System.out.println("No data for user id: " + user_id);
+
+			if (!found) {
+				System.out.println("No data for user id: " + user_id);
 			}
-						
+
 		} catch (SQLException e) {
 			System.out.println(e);
 		}
 	}
-
-
 
 }
--- src/GymRats/UserController.java
+++ src/GymRats/UserController.java
@@ -6,7 +6,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 public class UserController {
 	String connectionUrl;
@@ -15,33 +17,66 @@ public class UserController {
 
 	public UserController(String conn, String usrnm, String psswrd) throws ClassNotFoundException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		this.connectionUrl = conn; // could be different
-		this.dbUsername = usrnm; // replace with your username (most likely "root")
-		this.dbPassword = psswrd; // replace with your password
+		this.connectionUrl = conn;
+		this.dbUsername = usrnm;
+		this.dbPassword = psswrd;
 
 	}
 
-	public void getUsers() throws ClassNotFoundException {
+	public ArrayList<Dictionary<String, String>> getUsers() throws ClassNotFoundException {
 		String sqlSelectAllPersons = "SELECT * FROM user";
 		try (Connection conn = DriverManager.getConnection(this.connectionUrl, this.dbUsername, this.dbPassword);
 				PreparedStatement ps = conn.prepareStatement(sqlSelectAllPersons);
 				ResultSet rs = ps.executeQuery()) {
+			ArrayList<Dictionary<String, String>> ret = new ArrayList<Dictionary<String, String>>();
 			while (rs.next()) {
-				int id = rs.getInt("User_Id");
-				String name = rs.getString("FName");
-				String lastName = rs.getString("LName");
-				System.out.println("User #" + id + ": " + name + " " + lastName);
+				Dictionary<String, String> sub = new Hashtable<String, String>();
+				sub.put("User_id", Integer.toString(rs.getInt("User_Id")));
+				sub.put("FName", rs.getString("FName"));
+				sub.put("LName", rs.getString("LName"));
+				sub.put("Username", rs.getString("Username"));
+				sub.put("Password", rs.getString("Password"));
+				if (rs.getString("Bio") == null) {
+					sub.put("Bio", "null");
+				} else {
+					sub.put("Bio", rs.getString("Bio"));
+				}
+				if (rs.getString("FK_Trainer_Id") == null) {
+					sub.put("FK_Trainer_Id", "null");
+				} else {
+					sub.put("FK_Trainer_Id", rs.getString("FK_Trainer_Id"));
+				}
+
+				ret.add(sub);
 			}
+			return ret;
 		} catch (SQLException e) {
 			System.out.println(e);
 		}
+		return null;
 	}
 
-	public void addUser(String Fname, String Lname, String username, String password, String Bio, String trainer)
-			throws ClassNotFoundException {
-		String sqlSelectAllPersons = "INSERT INTO user (FName, LName, Username, Password, Bio, FK_Trainer_Id) VALUES ('"
-				+ Fname + "', '" + Lname + "', '" + username + "', '" + password + "', '" + Bio + "', '" + trainer
-				+ "')";
+	public void addUser(Dictionary<String, String> userInfo) throws ClassNotFoundException {
+		String sqlSelectAllPersons = null;
+		if (userInfo.get("Bio") != null && userInfo.get("FK_Trainer_Id") != null) {
+			sqlSelectAllPersons = "INSERT INTO user (FName, LName, Username, Password, Bio, FK_Trainer_Id) VALUES ('"
+					+ userInfo.get("FName") + "', '" + userInfo.get("LName") + "', '" + userInfo.get("Username")
+					+ "', '" + userInfo.get("Password") + "', '" + userInfo.get("Bio") + "', '"
+					+ userInfo.get("Fk_Trainer_Id") + "')";
+		} else if (userInfo.get("Bio") != null) {
+			sqlSelectAllPersons = "INSERT INTO user (FName, LName, Username, Password, Bio) VALUES ('"
+					+ userInfo.get("FName") + "', '" + userInfo.get("LName") + "', '" + userInfo.get("Username")
+					+ "', '" + userInfo.get("Password") + "', '" + userInfo.get("Bio") + "')";
+		} else if (userInfo.get("FK_Trainer_Id") != null) {
+			sqlSelectAllPersons = "INSERT INTO user (FName, LName, Username, Password,FK_Trainer_Id) VALUES ('"
+					+ userInfo.get("FName") + "', '" + userInfo.get("LName") + "', '" + userInfo.get("Username")
+					+ "', '" + userInfo.get("Password") + "', '" + userInfo.get("Fk_Trainer_Id") + "')";
+		} else {
+			sqlSelectAllPersons = "INSERT INTO user (FName, LName, Username, Password) VALUES ('"
+					+ userInfo.get("FName") + "', '" + userInfo.get("LName") + "', '" + userInfo.get("Username")
+					+ "', '" + userInfo.get("Password") + "')";
+		}
+
 		try (Connection conn = DriverManager.getConnection(this.connectionUrl, this.dbUsername, this.dbPassword);
 				PreparedStatement ps = conn.prepareStatement(sqlSelectAllPersons);) {
 			try {
@@ -56,27 +91,31 @@ public class UserController {
 		}
 	}
 
-	public boolean getUserByUsername(String username) throws ClassNotFoundException {
-		String sqlSelectAllPersons = "SELECT User_Id FROM user WHERE '" + username + "' IN (Username)";
+	public Dictionary<String, String> getUser(int userId) throws ClassNotFoundException {
+		String sqlSelectAllPersons = "SELECT * FROM user WHERE User_Id = " + userId;
 		try (Connection conn = DriverManager.getConnection(this.connectionUrl, this.dbUsername, this.dbPassword);
 				PreparedStatement ps = conn.prepareStatement(sqlSelectAllPersons);
 				ResultSet rs = ps.executeQuery()) {
 			if (rs.next()) {
-				return true;
+				Dictionary<String, String> ret = new Hashtable<String, String>();
+				ret.put("User_id", Integer.toString(rs.getInt("User_Id")));
+				ret.put("FName", rs.getString("FName"));
+				ret.put("LName", rs.getString("LName"));
+				ret.put("Username", rs.getString("Username"));
+				ret.put("Password", rs.getString("Password"));
+				ret.put("Bio", rs.getString("Bio"));
+				ret.put("FK_Trainer_Id", rs.getString("FK_Trainer_Id"));
+				return ret;
 			}
-			return false;
+			return null;
 		} catch (SQLException e) {
 			System.out.println(e);
-			return false;
+			return null;
 		}
 	}
 
-	public void deleteUser() throws ClassNotFoundException {
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter User Id to remove: ");
-		int deleted = scan.nextInt();
-		scan.close();
-		String sqlSelectAllPersons = "DELETE FROM user WHERE User_Id = " + deleted;
+	public void deleteUserByUserId(int userId) throws ClassNotFoundException {
+		String sqlSelectAllPersons = "DELETE FROM user WHERE User_Id = " + userId;
 		try (Connection conn = DriverManager.getConnection(this.connectionUrl, this.dbUsername, this.dbPassword);
 				PreparedStatement ps = conn.prepareStatement(sqlSelectAllPersons);) {
 			ps.execute();
@@ -85,4 +124,23 @@ public class UserController {
 		}
 	}
 
+	public void deleteUserByUsername(String username) throws ClassNotFoundException {
+		int userId = getUserByUsername(username);
+		deleteUserByUserId(userId);
+	}
+
+	private int getUserByUsername(String username) throws ClassNotFoundException {
+		String sqlSelectAllPersons = "SELECT * FROM user WHERE '" + username + "' IN (Username)";
+		try (Connection conn = DriverManager.getConnection(this.connectionUrl, this.dbUsername, this.dbPassword);
+				PreparedStatement ps = conn.prepareStatement(sqlSelectAllPersons);
+				ResultSet rs = ps.executeQuery()) {
+			if (rs.next()) {
+				return rs.getInt("User_Id");
+			}
+			return -1;
+		} catch (SQLException e) {
+			System.out.println(e);
+			return -1;
+		}
+	}
 }
